516afa334697dc0c0a0af8aae2d9b153eb99c257
henokgetu473-eng <henokgetu473@gmail.com>
2026-07-12 18:16:52 +0300

    fix: revert back to bot.launch

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 // ቦቱን ለማስነሳት
-bot.start()
+bot.launch()
   .then(() => console.log('🚀 ቴሌግራም ቦት በተሳካ ሁኔታ ተነስቷል!'))
   .catch((err) => console.error('❌ የቦት ስህተት፦', err));
 
